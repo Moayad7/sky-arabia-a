ac514ee5bd39e5721697e022eab4836c52c6f2e6
Moayad7 <mouayad.ahmad7@gmail.com>
2023-10-09 22:33:36 +0300

    0

--- js/script.js
+++ js/script.js
@@ -3,38 +3,36 @@ var logo1 = document.getElementById("logo1");
 var logo2 = document.getElementById("logo2");
 var toggle = document.getElementById("navbar-toggler");
 
+window.onscroll = function () {
+  scrollFunction();
+};
 
-window.onscroll = function() {scrollFunction()};
-    
 function scrollFunction() {
-    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-        document.getElementById("navbar").classList.add("scrolled");
-        myElement.classList.add("scrolled1");
-        myElement.classList.remove("scrolled2");
-        toggle.classList.add("scrolled3");
-        toggle.classList.remove("scrolled4");
-        logo1.classList.add("hidden");
-        logo1.classList.remove("block");
-        logo2.classList.add("block");
-        logo2.classList.remove("hidden");
-    } else {
-        document.getElementById("navbar").classList.remove("scrolled");
-        myElement.classList.remove("scrolled1");
-        myElement.classList.add("scrolled2");
-        toggle.classList.add("scrolled4");
-        toggle.classList.remove("scrolled3");
-        logo1.classList.add("block");
-        logo1.classList.remove("hidden");
-        logo2.classList.add("hidden");
-        logo2.classList.remove("block");
-    }
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("navbar").classList.add("scrolled");
+    myElement.classList.add("scrolled1");
+    myElement.classList.remove("scrolled2");
+    toggle.classList.add("scrolled3");
+    toggle.classList.remove("scrolled4");
+    logo1.classList.add("hidden");
+    logo1.classList.remove("block");
+    logo2.classList.add("block");
+    logo2.classList.remove("hidden");
+  } else {
+    document.getElementById("navbar").classList.remove("scrolled");
+    myElement.classList.remove("scrolled1");
+    myElement.classList.add("scrolled2");
+    toggle.classList.add("scrolled4");
+    toggle.classList.remove("scrolled3");
+    logo1.classList.add("block");
+    logo1.classList.remove("hidden");
+    logo2.classList.add("hidden");
+    logo2.classList.remove("block");
+  }
 }
 logo2.classList.add("hidden");
 
-
 AOS.init();
-
-
 
 // function myFunction() {
 //     var x = document.getElementById("myTopnav");
@@ -45,35 +43,39 @@ AOS.init();
 //     }
 //   }
 
-toggle.onclick = function(){Menu()}
+toggle.onclick = function () {
+  Menu();
+};
 
-
-
-  function Menu() {
-    let item = toggle
-    let list = document.querySelector('ul')
-    item.name === 'menu' ? (item.name = 'close') : (item.name = 'menu')
-    item.name === 'close' ? (list.classList.add('left-[0px]')) : (list.classList.remove('left-[0px]'))
-    console.log("toggle");
+function Menu() {
+  let item = toggle;
+  let list = document.querySelector("ul");
+  item.name === "menu" ? (item.name = "close") : (item.name = "menu");
+  item.name === "close"
+    ? list.classList.add("left-[0px]")
+    : list.classList.remove("left-[0px]");
+  console.log("toggle");
 }
-
 
 var loader = document.getElementsByClassName("loader");
 var content = document.querySelector(".content");
 
-window.addEventListener("load", ()=>{
-    this.setTimeout(()=>{
-  document.querySelector(".spinnerContainer").classList.add("loader1--hidden")
-  document.querySelector(".spinnerContainer").classList.remove("loader1--visible")
-    },1700)
-    this.setTimeout(()=>{
-        document.querySelector(".spinnerContainer").classList.add("hidden")
-          },2700)
+window.addEventListener("load", () => {
+  this.setTimeout(() => {
+    document
+      .querySelector(".spinnerContainer")
+      .classList.add("loader1--hidden");
+    document
+      .querySelector(".spinnerContainer")
+      .classList.remove("loader1--visible");
+  }, 1700);
+  this.setTimeout(() => {
+    document.querySelector(".spinnerContainer").classList.add("hidden");
+  }, 2700);
 });
 
-
 // var load = window.addEventListener("load",()=>{
-    
+
 //     document.querySelector(".loader1").classList.add("loader1--hidden")
 //   })
 // setInterval(load,50000)
