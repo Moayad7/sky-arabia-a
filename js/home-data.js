1b2054109aa6559233c4acd789d205d3286bbd90
Moayad7 <mouayad.ahmad7@gmail.com>
2023-10-16 12:56:29 +0300

    Update home-data.js

--- js/home-data.js
+++ js/home-data.js
@@ -129,26 +129,25 @@ function displayServices() {
 //   show projects data
 function displayProjects() {
     const ProjectsList = document.getElementById("projects-list");
-    projects.map((project, i, array) => {
-      if (i < 5) {
-        const projectItem = document.createElement("a");
-      projectItem.setAttribute("href", "#");
-      projectItem.setAttribute("data-aos", "fade-up");
-      projectItem.className =
-        "proj-card h-96 w-full lg:w-[500px] text-xl font-bold";
-      projectItem.innerHTML = `
-       <div class="proj-pic${i + 1} flex relative items-end">
-         <h3
-           class="proj-title text-[#fff] absolute text-center w-full lg:text-3xl bg-[#0044bb55] mb-0 py-2"
-         >
-           ${project.title}
-         </h3>
-       </div>
-       `;
-      ProjectsList.appendChild(projectItem);
-      }
-      
-    });
+    projects.forEach((project, i, array) => {
+        if (i < 5) {
+          const projectItem = document.createElement("a");
+        projectItem.setAttribute("href", "#");
+        projectItem.setAttribute("data-aos", "fade-up");
+        projectItem.className =
+          "proj-card h-96 w-full lg:w-[500px] text-xl font-bold";
+        projectItem.innerHTML = `
+         <div class="proj-pic${i + 1} flex relative items-end">
+           <h3
+             class="proj-title text-[#fff] absolute text-center w-full lg:text-3xl bg-[#0044bb55] mb-0 py-2"
+           >
+             ${project.title}
+           </h3>
+         </div>
+         `;
+        ProjectsList.appendChild(projectItem);
+        }
+    });    
   }
   
   
