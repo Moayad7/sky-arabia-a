ba4435c53e8143d01f20b2b7c0041fc931647a69
Moayad7 <mouayad.ahmad7@gmail.com>
2023-10-16 16:26:05 +0300

    Update projects-data.js

--- js/projects-data.js
+++ js/projects-data.js
@@ -1,29 +1,28 @@
-
 async function fetchProjectsData() {
-    try {
-      const response = await fetch(projects_url);
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      const data = await response.json();
-      console.log("Data:", data);
-      projects = data;
-      displayProjects();
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
+  try {
+    const response = await fetch(projects_url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
-  }
+    const data = await response.json();
+    console.log("Data:", data);
+    projects = data;
 
-  
+    displayProjects();
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
 //   show projects data
 function displayProjects() {
-    const ProjectsList = document.getElementById("projects-list");
-    projects.forEach((project, i) => {
-      const projectItem = document.createElement("a");
-      projectItem.setAttribute("href", "#");
-      projectItem.className =
-        "proj-card h-96 w-full lg:w-[500px] text-xl font-bold";
-      projectItem.innerHTML = `
+  const ProjectsList = document.getElementById("projects-list");
+  projects.forEach((project, i) => {
+    const projectItem = document.createElement("a");
+    projectItem.setAttribute("href", "#");
+    projectItem.className =
+      "proj-card h-96 w-full lg:w-[500px] text-xl font-bold";
+    projectItem.innerHTML = `
        <div class="proj-pic${i + 1} flex relative items-end">
          <h3
            class="proj-title text-[#fff] absolute text-center w-full lg:text-3xl bg-[#0044bb55] mb-0 py-2"
@@ -32,10 +31,8 @@ function displayProjects() {
          </h3>
        </div>
        `;
-      ProjectsList.appendChild(projectItem);
-    });
-  }
-  
+    ProjectsList.appendChild(projectItem);
+  });
+}
 
-
-  fetchProjectsData();
+fetchProjectsData();
